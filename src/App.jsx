8ae8d9649a94b6a4532208be3d0a3fa84f5c33ca
Thangga7 <beencules@gmail.com>
2024-12-03 00:03:00 +0700

    hjhj

--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,21 @@ import Login from "./components/Layout/Login/Login";
 import Register from "./components/Layout/Register/Register";
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [isRegistering, setIsRegistering] = useState(false);
+  const [isRegistering, setIsRegistering] = useState(true);
 
   return (
     <>
       {!isAuthenticated ? (
         isRegistering ? (
           <div
-            className="text-white h-full flex justify-center items-center bg-cover"
+            className="text-white h-[100vh] flex justify-center items-center bg-cover"
             style={{ backgroundImage: "url('../src/assets/bg.png')" }}
           >
             <Register setIsAuthenticated={setIsAuthenticated} />
           </div>
         ) : (
           <div
-            className="text-white h-full flex justify-center items-center bg-cover"
+            className="text-white h-[100vh] flex justify-center items-center bg-cover"
             style={{ backgroundImage: "url('../src/assets/bg.png')" }}
           >
             <Login setIsAuthenticated={setIsAuthenticated} />
