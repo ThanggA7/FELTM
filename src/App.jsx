62afb1f8ae9785d53f16386298adc97b5bd6d865
Thangga7 <beencules@gmail.com>
2024-12-03 00:02:08 +0700

    hjhj

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,43 @@ import React, { useState } from "react";
 import Header from "./components/Layout/Header/Header";
 import { Route, Routes } from "react-router-dom";
 import PublicRouter from "./Routes";
-
+import Login from "./components/Layout/Login/Login";
+import Register from "./components/Layout/Register/Register";
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isRegistering, setIsRegistering] = useState(false);
 
   return (
     <>
-      <Header />
-      <div className="container mx-auto p-4">
-        <Routes>
-          {PublicRouter.map((route, index) => {
-            const Page = route.component;
-            return <Route key={index} path={route.path} element={<Page />} />;
-          })}
-        </Routes>
-      </div>
+      {!isAuthenticated ? (
+        isRegistering ? (
+          <div
+            className="text-white h-full flex justify-center items-center bg-cover"
+            style={{ backgroundImage: "url('../src/assets/bg.png')" }}
+          >
+            <Register setIsAuthenticated={setIsAuthenticated} />
+          </div>
+        ) : (
+          <div
+            className="text-white h-full flex justify-center items-center bg-cover"
+            style={{ backgroundImage: "url('../src/assets/bg.png')" }}
+          >
+            <Login setIsAuthenticated={setIsAuthenticated} />
+          </div>
+        )
+      ) : (
+        <div className="container mx-auto p-4">
+          (<Header />
+          )(
+          <Routes>
+            {PublicRouter.map((route, index) => {
+              const Page = route.component;
+              return <Route key={index} path={route.path} element={<Page />} />;
+            })}
+          </Routes>
+          )
+        </div>
+      )}
     </>
   );
 }
